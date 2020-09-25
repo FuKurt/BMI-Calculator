4525432c8a27da6da74fe2d59893e47ba4b2ee9b
Furkan Kurt <furkan.kurt.13@outlook.com>
2020-09-25 14:43:25 +0300

    Created BMI equation and made labels to show current value of sliders

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        let heightValue = String(format: "%.2f",  sender.value)
+        heightLabel.text = "\(heightValue)m"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let weightValue = String(format: "%.0f",  sender.value)
+        weightLabel.text = "\(weightValue)kg"
+    }
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        let bmi = weight / pow(height, 2)
+        print(bmi)
+    }
+    
 }
 

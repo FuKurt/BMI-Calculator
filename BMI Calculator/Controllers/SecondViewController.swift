659068d3be87ce8ec3749436db72d64bf98df61e
Furkan Kurt <furkan.kurt.13@outlook.com>
2020-09-25 16:09:57 +0300

    SecondViewController is created programmatically and info inter-transfer is established

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Macbook on 25.09.2020.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    var bmiValue = "0.0"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //creating view programmatically
+        view.backgroundColor = .red
+        
+        let label = UILabel()
+        label.text = bmiValue
+        label.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        
+        view.addSubview(label)
+        
+    }
+}
--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -34,6 +34,10 @@ class ViewController: UIViewController {
         let weight = weightSlider.value
         let bmi = weight / pow(height, 2)
         print(bmi)
+        
+        let secondVC = SecondViewController()
+        secondVC.bmiValue = String(format: "%.1f", bmi)
+        self.present(secondVC, animated: true, completion: nil)
     }
     
 }
